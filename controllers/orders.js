ad01cda54e60324cc4f4d362b911df8b0a389a2d
Moloto <molotomarkharms@gmail.com>
2021-02-04 00:56:33 +0200

    First

--- controllers/orders.js
+++ controllers/orders.js
@@ -42,48 +42,5 @@ router.get('/allOrders', (req, res,next) => {
 
     })
 });
-router.post('/addOrder/', (req, res, next) => {
-     debugger;
-    return new Promise((resolve, reject) => {
-        let placeholder = '';
-        let count = 1;
-        const params = Object.keys(req.body).map(key => [(key), req.body[key]]);
 
-        const paramsValues = Object.keys(req.body).map(key => req.body[key]);
-
-        if (Array.isArray(params)) {
-            params.forEach(() => {
-                placeholder += `$${count},`;
-                count += 1;
-            });
-        } 
-
-        placeholder = placeholder.replace(/,\s*$/, ''); 
-
-        const functionName = `fn_add_new_order`;
-
-        const sql = `${functionName}(${placeholder})`;
-
-        postgres.callFnWithResultsAdd(sql, paramsValues)
-        .then((data) => {
-            debugger;
-            res.status(201).json({
-                message: 'Successfully Added ORDER',
-                addedUser: data,
-                status: true
-            });
-            resolve(data);
-
-        })
-        .catch((error) => {
-            debugger;
-            res.status(500).json({
-                message: 'bad Request',
-                error: error,
-                status: false
-            });
-            reject(error);
-        })
-    })
-});
 module.exports = router;
