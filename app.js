171d4adc15ff1d270cd78cca1db42850a88f8942
Moloto <molotomarkharms@gmail.com>
2021-02-03 23:59:17 +0200

    Add order

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 
 const Database = require('./database');
 const customer = require('./controllers/costomers')
+const orders = require('./controllers/orders')
 
 router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.json());
@@ -13,6 +14,7 @@ router.use(cors());
 var postgres = new Database()
 //var cust = new Customer
 router.use('/customer',customer)
+router.use('/order',customer)
 
 //HELLO WORLD=================================================================================================
 router.get('/', (req, res) => res.send('Hello World!'))
---
+++ controllers/orders.js
@@ -0,0 +1,45 @@
+const express = require('express');
+
+const router = express();
+const cors = require('cors');
+const bodyParser = require('body-parser')
+
+const Database = require('../database');
+
+router.use(bodyParser.urlencoded({ extended: false }));
+router.use(bodyParser.json());
+router.use(cors());
+var postgres = new Database()
+
+//GET ALL ORDERS===========================================================================================
+router.get('/allcustomers', (req, res,next) => {
+   
+ debugger;
+    const functionName = `fn_get_all_orders`;
+
+    return new Promise((resolve, reject) => {
+
+        postgres.functionWithResults(functionName)
+            .then((data) => {
+                debugger;
+                res.status(200).json({
+                    message: 'Here is all appointments',
+                    appointments: data,
+                    status: true
+                });
+                resolve(data);
+
+            })
+            .catch((error => {
+                debugger;
+                res.status(500).json({
+                    message: 'bad Request',
+                    error: error,
+                    status: false
+                });
+                reject(error);
+            }))
+
+    })
+});
+module.exports = router;
