0532b9426cc5698837a848498e7b97846eddc00b
Moloto <molotomarkharms@gmail.com>
2021-02-04 10:21:51 +0200

    Commit

--- app.js
+++ app.js
@@ -3,15 +3,12 @@ const express = require('express');
 const router = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
-
-const Database = require('./database');
 const customer = require('./controllers/costomers')
 const orders = require('./controllers/orders')
 
 router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.json());
 router.use(cors());
-var postgres = new Database()
 //var cust = new Customer
 router.use('/customer',customer)
 router.use('/order',orders)
