a762e4a44216dfbff2e458fe053683517ee29ecb
Moloto <molotomarkharms@gmail.com>
2021-02-04 22:30:57 +0200

    make it better

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const customer = require('./controllers/costomers')
 const orders = require('./controllers/orders')
+const items = require('./controllers/items')
 
 router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.json());
@@ -12,6 +13,7 @@ router.use(cors());
 //var cust = new Customer
 router.use('/customer',customer)
 router.use('/order',orders)
+router.use('/items',items)
 
 //HELLO WORLD=================================================================================================
 router.get('/', (req, res) => res.send('Hello World!'))
